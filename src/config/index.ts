4a3aa8df09c9ff890d41176fbf50d81ede39fe20
oluwaferanmiadetunji <oluwaferanmiadetunji@gmail.com>
2021-08-25 15:08:45 +0100

    .

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,6 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 export const MONGO_URI = process.env.MONGO_URI || '';
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const JWT_ACCESS_EXPIRATION_MINUTES = process.env.JWT_ACCESS_EXPIRATION_MINUTES;
 export const TWILLIO_KEYS = {
 	accountSID: process.env.TWILIO_ACCOUNT_SID || '',
 	authToken: process.env.TWILIO_AUTH_TOKEN || '',
